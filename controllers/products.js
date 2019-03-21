b0a1ea1d3ec43593eea1c934b0c8aee24223c409
Koushik-G <koushikgelli@gmail.com>
2019-03-21 23:58:00 +0530

    changes made to store data in filesystem

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+const Product = require('../models/product');
+
+module.exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'AddProduct',
+    path: '/admin/add-product',
+    activeAddProduct: true,
+    formCSS: true,
+    productCSS: true
+  });
+};
+
+module.exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+module.exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+  
+};
+
